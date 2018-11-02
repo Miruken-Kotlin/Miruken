2c09365f070094dab169a2b9a67dfb1744f43557
Craig Neuwirt <cneuwirt@gmail.com>
2018-11-02 10:52:21 -0500

    Added MirukenModule for Jackson

--- miruken/src/test/kotlin/com/miruken/api/JacksonHelper.kt
+++ miruken/src/test/kotlin/com/miruken/api/JacksonHelper.kt
@@ -8,47 +8,55 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver
 import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase
+import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlin.reflect.KType
-
-typealias JacksonNamedType =
-        com.fasterxml.jackson.databind.jsontype.NamedType
 
 object JacksonHelper {
     val json: ObjectMapper = jacksonObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-            .addMixIn(NamedType::class.java, NamedTypeMixin::class.java)
-            .addMixIn(KType::class.java, IgnoreTypeMixin::class.java)
             .registerModule(JavaTimeModule())
+            .registerModule(MirukenModule)
 
-    @JsonIgnoreType
-    interface IgnoreTypeMixin
+    object MirukenModule : SimpleModule() {
+        init {
+            setMixInAnnotation(NamedType::class.java,
+                    NamedTypeMixin::class.java)
+            setMixInAnnotation(KType::class.java,
+                    IgnoreTypeMixin::class.java)
+        }
 
-    @JsonTypeInfo(
-            use      = JsonTypeInfo.Id.CUSTOM,
-            include  = JsonTypeInfo.As.PROPERTY,
-            property = "\$type")
-    @JsonTypeIdResolver(NamedTypeIdResolver::class)
-    interface NamedTypeMixin {
-        @get:JsonIgnore
-        val typeName: String
-    }
+        @JsonIgnoreType
+        interface IgnoreTypeMixin
 
-    object NamedTypeIdResolver : TypeIdResolverBase() {
-        override fun idFromValue(value: Any): String? =
-            when (value) {
-                is NamedType -> value.typeName
-                else -> idFromBaseType()
+        @JsonTypeInfo(
+                use      = JsonTypeInfo.Id.CUSTOM,
+                include  = JsonTypeInfo.As.PROPERTY,
+                property = "\$type")
+        @JsonTypeIdResolver(NamedTypeIdResolver::class)
+        interface NamedTypeMixin {
+            @get:JsonIgnore
+            val typeName: String
+        }
+
+        object NamedTypeIdResolver : TypeIdResolverBase() {
+            override fun idFromValue(value: Any): String? {
+                val typeName = (value as? NamedType)?.typeName
+                check(!typeName.isNullOrBlank()) {
+                    "${value::class.qualifiedName} requires a valid typeName"
+                }
+                return typeName
             }
 
-        override fun idFromValueAndType(
-                value: Any,
-                suggestedType: Class<*>?) = idFromValue(value)
+            override fun idFromValueAndType(
+                    value: Any,
+                    suggestedType: Class<*>?) = idFromValue(value)
 
-        // TODO: override typeFromId to map .Net $type to java type
+            // TODO: override typeFromId to map .Net $type to java type
 
-        override fun getMechanism() = JsonTypeInfo.Id.CUSTOM
+            override fun getMechanism() = JsonTypeInfo.Id.CUSTOM
+        }
     }
 }
