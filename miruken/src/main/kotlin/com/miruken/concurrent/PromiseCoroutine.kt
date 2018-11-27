c80f19a97701428848ac55905517f0f102402bd0
Craig Neuwirt <cneuwirt@gmail.com>
2018-11-26 21:41:33 -0600

    Minor code cleanup

--- miruken/src/main/kotlin/com/miruken/concurrent/PromiseCoroutine.kt
+++ miruken/src/main/kotlin/com/miruken/concurrent/PromiseCoroutine.kt
@@ -8,6 +8,6 @@ import kotlin.coroutines.resumeWithException
 suspend fun <T> Promise<T>.await(): T =
         suspendCancellableCoroutine { cont: CancellableContinuation<T> ->
             cont.invokeOnCancellation { cancel() }
-            then({ cont.resume(it) }, { cont.resumeWithException(it) })
+            then(cont::resume, cont::resumeWithException)
             cancelled { cont.cancel(it) }
         }
