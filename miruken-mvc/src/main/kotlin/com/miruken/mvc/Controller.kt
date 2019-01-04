ef8b5a9c67a2aca4b701ebcc61847121a2e751bf
Craig Neuwirt <cneuwirt@gmail.com>
2019-01-04 11:52:19 -0600

    Added Controller.finish for workflow support

--- miruken-mvc/src/main/kotlin/com/miruken/mvc/Controller.kt
+++ miruken-mvc/src/main/kotlin/com/miruken/mvc/Controller.kt
@@ -146,6 +146,42 @@ abstract class Controller : Contextual, AutoCloseable {
 
     protected fun goBack(handler: Handling) = handler.goBack()
 
+    // Workflow
+
+    fun finish() {
+        endContext()
+    }
+
+    inline fun <reified A: Any> finish(a: A) {
+        context?.also {
+            it.store(a).end()
+        }
+    }
+
+    inline fun <reified A1: Any, reified A2: Any> finish(a1: A1, a2: A2) {
+        context?.also {
+            it.store(a1).store(a2).end()
+        }
+    }
+
+    inline fun <reified A1: Any, reified A2: Any, reified A3: Any> finish(a1: A1, a2: A2, a3: A3) {
+        context?.also {
+            it.store(a1).store(a2).store(a3).end()
+        }
+    }
+
+    inline fun <reified A1: Any, reified A2: Any, reified A3: Any, reified A4: Any> finish(a1: A1, a2: A2, a3: A3, a4: A4) {
+        context?.also {
+            it.store(a1).store(a2).store(a3).store(a4).end()
+        }
+    }
+
+    inline fun <reified A1: Any, reified A2: Any, reified A3: Any, reified A4: Any, reified A5: Any> finish(a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) {
+        context?.also {
+            it.store(a1).store(a2).store(a3).store(a4).store(a5).end()
+        }
+    }
+
     protected fun delay(delayMs: Long) =
             Promise.delay(delayMs).also { context?.track(it) }
 
